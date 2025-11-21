a2c4cdd2fa3a46a782222ec4adeed91abc95f793
Eric Ehninger <mailuntoeric@gmail.com>
2025-11-20 23:08:43 -0700

    SQL to populate db with dummy data

---
+++ src/sql/dummy_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    teams ("id", "name")
+VALUES
+    (1, 'East Coast Team'),
+    (2, 'West Coast Team');
+
+INSERT INTO
+    employees ("id", "name", "team")
+VALUES
+    (1, 'Boston Alice', 1),
+    (2, 'Seattle Bob', 2);
